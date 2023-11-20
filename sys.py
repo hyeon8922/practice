a240f3d08cd0ca162fb39fc5f8e5eee88c428d5a
hyeon8922 <81852715+hyeon8922@users.noreply.github.com>
2023-11-20 23:41:44 +0900

    Create sys.py

---
+++ sys.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+time = [[0]*2 for _ in range(N)]
+for i in range(N):
+    s, e = map(int, sys.stdin.readline().split())
+    time[i][0] = s
+    time[i][1] = e
+print(time)
